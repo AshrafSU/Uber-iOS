8b1c8feae6327f72e18575cae6cb3a64dd4e7a82
Ashraful Islam <mannabteb@gmail.com>
2022-11-05 13:21:43 +0600

    Initial Commit

---
+++ Uber iOS/Uber_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_iOSApp.swift
+//  Uber iOS
+//
+//  Created by Ashraful Islam on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Uber_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
